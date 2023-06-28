6b6f8c39a118e673db16eac3ea651a11c2f5a774
LAUTARO FERNANDO YUKELSON <47434208@ort>
2023-06-28 11:53:32 -0300

    Creo TP - Creo base de datos y script - .gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP_Elecciones.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
