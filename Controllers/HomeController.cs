b74220a7374baf30a4f3c8cb86365549fe4d2cf8
LAUTARO FERNANDO YUKELSON <47434208@ort>
2023-06-29 14:38:07 -0300

    Crew view y actions en el controller. Listo para el diseño

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,24 +8,32 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
+    public HomeController(ILogger<HomeController> logger){
         _logger = logger;
     }
 
-    public IActionResult Index()
-    {
+    public IActionResult Index(){
         return View();
     }
 
-    public IActionResult Privacy()
-    {
+    public IActionResult VerDetalleCandidato(){
+        return View();   
+    }
+
+    public IActionResult VerDetallePartido(){
+        return View();
+    }
+
+    public IActionResult AgregarCandidato(){
+        return View();
+    }
+
+    public IActionResult AgregarPartido(){
         return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
+    public IActionResult Error(){
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
